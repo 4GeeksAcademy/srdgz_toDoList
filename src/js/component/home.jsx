64c722a057b9e618f44478a40c98f1df4bd415ab
Sandra Rodríguez <133523307+srdgz@users.noreply.github.com>
2023-08-18 14:33:49 +0000

    adding useEffect

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,50 +3,88 @@ import React, { useEffect, useState } from "react";
 const Home = () => {
   const [inputValue, setInputValue] = useState({ label: "", done: false });
   const [todos, setTodos] = useState([]);
+  const updatedTodoList = [...todos, inputValue];
   const URL = "https://playground.4geeks.com/apis/fake/todos/user/srdgz";
 
-  /*
-	useEffect(() => {
-		fetch(`${URL}`, {
-			method: "POST",
-			mode: "cors",
-			redirect: "follow",
-			body: JSON.stringify([]),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		}) 
-		.then((res) => res.json())
-		.then((res) => setTodos(res))
-		.catch(err => console.log(err));
-	}, []);
+  const getTask = () => {
+    fetch(`${URL}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          let data = response.json();
+          setTodos(data);
+        } else {
+          newUser();
+        }
+      })
+      .catch((error) => console.log("Something went wrong: \n", error));
+  };
 
-	useEffect(() => {
-		fetch(`${URL}`)
-    	.then((res) => {
-        	if (!res.ok) {
-            throw Error(res.status);
-        	}
-       		return res.json();
-    	})
-		.then((res) => setTodos(res))
-		.catch(err => console.log(err));
-	}, []);
+  const newUser = () => {
+    fetch(`${URL}`, {
+      method: "POST",
+      body: JSON.stringify([]),
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) getTask();
+      })
+      .catch((error) => console.log("Something went wrong: \n", error));
+  };
 
-	useEffect(() => {
-		if (!todos.length) return;
-		fetch(`${URL}`, {
-			method: "PUT",
-			body: JSON.stringify(todos),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-		.then(() => console.log("Everything is ok"))
-		.catch(() => console.log("Something went wrong"));
-	  }, [todos]);
+  const handleKey = (task) => {
+    if (task.key === "Enter" && inputValue.label.trim() !== "") {
+      addTask();
+    }
+  };
 
-*/
+  const addTask = () => {
+    if (todos.find((elem) => elem.label.trim() === inputValue.label.trim())) {
+      alert("The field cannot be duplicated");
+      return;
+    }
+    fetch(`${URL}`, {
+      method: "PUT",
+      body: JSON.stringify(updatedTodoList),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.status + " " + response.statusText);
+        } else {
+          setTodos(updatedTodoList);
+          setInputValue({ label: "", done: false });
+        }
+      })
+      .catch((error) => console.log("Something went wrong: \n", error));
+  };
+
+  const handleDelete = (index) => {
+    const newList = todos.filter((i, currentIndex) => index != currentIndex);
+    fetch(`${URL}`, {
+      method: "PUT",
+      body: JSON.stringify(newList),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.status + " " + response.statusText);
+        } else {
+          getTask();
+        }
+      })
+      .catch((error) => console.log("Something went wrong: \n", error));
+  };
 
   const deleteAllUser = () => {
     fetch(`${URL}`, {
@@ -54,14 +92,21 @@ const Home = () => {
       headers: {
         "Content-type": "application/json",
       },
-    });
-    setTodos([]);
-    setTimeout(() => {
-      alert("All tasks were successfully deleted");
-    }, "500").catch((error) =>
-      console.error("Something went wrong. Error: ", error)
-    );
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.status + " " + response.statusText);
+        } else {
+          setTodos([]);
+          alert("All tasks were successfully deleted");
+        }
+      })
+      .catch((error) => console.log("Something went wrong: \n", error));
   };
+
+  useEffect(() => {
+    getTask();
+  }, []);
 
   return (
     <div className="container col-10">
@@ -78,37 +123,17 @@ const Home = () => {
             onChange={(task) =>
               setInputValue({ ...inputValue, label: task.target.value })
             }
-            onKeyUp={(task) => {
-              if (task.key === "Enter") {
-                if (inputValue.label.trim() === "") {
-                  alert("The field cannot be empty");
-                  return;
-                } else if (
-                  todos.find(
-                    (elem) => elem.label.trim() === inputValue.label.trim()
-                  )
-                ) {
-                  alert("The field cannot be duplicated");
-                  return;
-                }
-                const updatedTodoList = [...todos, inputValue];
-                setTodos(updatedTodoList);
-                setInputValue({ label: "", done: false });
-              }
-            }}
+            onKeyUp={handleKey}
           ></input>
         </li>
+
         {todos.map((task, index) => (
           <li key={index}>
             {task.label}
             {""}
             <i
               className="fa-regular fa-circle-xmark"
-              onClick={() =>
-                setTodos(
-                  todos.filter((i, currentIndex) => index != currentIndex)
-                )
-              }
+              onClick={handleDelete(index)}
             ></i>
             <hr className="breakLine" />
           </li>
